43a54d093993f2b5be7d586d4b7f4c74d3551615
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2022-03-13 19:14:04 +0900

    Update models

--- api/croissant/models.py
+++ api/croissant/models.py
@@ -5,7 +5,7 @@ class Layer(models.Model):
     title = models.CharField(max_length=64, blank=True)
     text = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    # edited = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['created']
