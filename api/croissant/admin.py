7d034a7cb92420dceeec53224530039da792e7af
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2022-03-13 19:07:01 +0900

    Update admin

--- api/croissant/admin.py
+++ api/croissant/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from croissant.models import Layer
 
-# Register your models here.
+admin.site.register(Layer)
--- api/croissant/models.py
+++ api/croissant/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 
 class Layer(models.Model):
-    title = models.CharField(max_length=64, default='Untitled')
-    text = models.TextField()
+    title = models.CharField(max_length=64, blank=True)
+    text = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['created']
