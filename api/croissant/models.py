ebc320c26ca48b479647cb0ca50872144a7c1106
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2022-03-21 11:11:45 +0900

    Update serializers and views

--- api/croissant/models.py
+++ api/croissant/models.py
@@ -8,18 +8,28 @@ class Layer(models.Model):
     children = models.ManyToManyField('self', blank=True)
     tasks = models.PositiveIntegerField(default=1)
     progress = models.PositiveIntegerField(default=0)
-    owner = models.ForeignKey(get_user_model(), related_name='own', on_delete=models.CASCADE)  # 自身を自動で入れる
+    owner = models.ForeignKey(get_user_model(), related_name='own', on_delete=models.CASCADE)  # 自身を自動で入れる  なぜかIDでなくnameになる
     participants = models.ManyToManyField(get_user_model(), related_name='participate')  # デフォルトを自身に
     start_date = models.DateField(null=True)
     start_time = models.TimeField(null=True)
     end_date = models.DateField(null=True)
     end_time = models.TimeField(null=True)
     created = models.DateTimeField(auto_now_add=True)
-    edited = models.DateTimeField(auto_now_add=True)
-
+    edited = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
 
     class Meta:
         ordering = ['start_date']
+
+
+# class Child(models.Model):
+#     parent_id = models.ForeignKey(Layer, related_name='children', on_delete=models.CASCADE)
+#     child_id = models.ForeignKey(Layer, related_name='parents', on_delete=models.CASCADE)
+#     created = models.DateTimeField(auto_now_add=True)
+
+# class Tasks(models.Model):
+#     layer_id = models.ForeignKey(Layer, related_name='tasks', on_delete=models.CASCADE)
+#     amount = models.PositiveIntegerField(default=1)
+#     created = models.DateTimeField(auto_now_add=True)
--- api/croissant/views.py
+++ api/croissant/views.py
@@ -1,46 +1,8 @@
+from rest_framework import viewsets
 from croissant.models import Layer
 from croissant.serializers import LayerSerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 
 
-class LayersView(APIView):
-    def get(self, request, format=None):
-        layers = Layer.objects.all()
-        serializer = LayerSerializer(layers, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = LayerSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class LayerView(APIView):
-    def get_object(self, pk):
-        try:
-            return Layer.objects.get(pk=pk)
-        except Layer.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        layer = self.get_object(pk)
-        serializer = LayerSerializer(layer)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        layer = self.get_object(pk)
-        serializer = LayerSerializer(layer, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        layer = self.get_object(pk)
-        layer.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class LayerViewSet(viewsets.ModelViewSet):
+    serializer_class = LayerSerializer
+    queryset = Layer.objects.all()
