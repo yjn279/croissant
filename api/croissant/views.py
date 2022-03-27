6db775a43e6c14c34731b447ae48d3c85579e3ce
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2022-03-27 09:14:54 +0900

    Update LayerView.get

--- api/croissant/views.py
+++ api/croissant/views.py
@@ -14,11 +14,13 @@ class LayersView(APIView):
         data = []
         for layer in Layer.objects.all():
 
+            # Start
             start = StartSerializer(layer.start.latest('created')).data
             start['start_date'] = start['date']
             start['start_time'] = start['time']
             del start['id'], start['layer'], start['date'], start['time']
 
+            # Layer
             layer = LayerSerializer(layer).data
             del layer['start']
 
@@ -29,10 +31,12 @@ class LayersView(APIView):
 
     def post(self, request, format=None):
 
+        # Start
         start_date = request.data.pop('start_date')
         start_time = request.data.pop('start_time')
         request.data['start'] = [{'date': start_date, 'time': start_time}]
 
+        # Layer
         layer_serializer = LayerSerializer(data=request.data)
 
         if layer_serializer.is_valid():
@@ -53,26 +57,53 @@ class LayerView(APIView):
 
 
     def get(self, request, pk, format=None):
-        # layer = LayerSerializer(layer)
-        # data = layer.data
-        # data['start_date'] = data['start'][0]['date']
-        # data['starta_time'] = data['start'][0]['time']
-        # _ = data.pop('start')
-        # datas.append(data)
+        
         layer = self.get_object(pk)
-        start = layer.start.latest('created')
-        layer_serializer = LayerSerializer(layer)
-        start_serializer = StartSerializer(start)
-        return Response(start_serializer.data)
+
+        # Start
+        start = StartSerializer(layer.start.latest('created')).data
+        start['start_date'] = start['date']
+        start['start_time'] = start['time']
+        del start['id'], start['layer'], start['date'], start['time']
+
+        # Layer
+        layer = LayerSerializer(layer).data
+        del layer['start']
+
+        data = {**layer, **start}
+        return Response(data)
 
 
     def put(self, request, pk, format=None):
+
         layer = self.get_object(pk)
-        serializer = LayerSerializer(layer, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        # # Start
+        # start_date = request.data.pop('start_date')
+        # start_time = request.data.pop('start_time')
+        # request.data['start'] = [{'date': start_date, 'time': start_time}]
+
+        # # Layer
+        # layer_serializer = LayerSerializer(data=request.data)
+
+        # # Start
+        # start_date = request.data.pop('start_date')
+        # start_time = request.data.pop('start_time')
+        # start = [{'date': start_date, 'time': start_time}]
+        # start = StartSerializer(layer.start.latest('created')).data
+        # start['start_date'] = start['date']
+        # start['start_time'] = start['time']
+        # del start['id'], start['layer'], start['date'], start['time']
+
+        # # Layer
+        # layer = LayerSerializer(layer).data
+        # del layer['start']
+
+        # serializer = LayerSerializer(layer, data=request.data)
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     return Response(serializer.data)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
     def delete(self, request, pk, format=None):
