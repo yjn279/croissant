43d5d48e8f89b9b34b172570ab9655597e2904c4
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2022-04-11 20:57:37 +0900

    Create next.js app

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 ## Setup
 - python 3.9
 - Node.js 17.9
+- Material UI
+    - `npm install @mui/material @emotion/react @emotion/styled`
+    - `npm install @mui/material @mui/styled-engine-sc styled-components`
+    - `npm install @mui/icons-material`
+    - `npm install @mui/x-date-pickers`
+    - run: `npx @mui/codemod v5.0.0/date-pickers-moved-to-x <path>`
+    - DateTime: `npm install @date-io/date-fns`
+    - axios: `npm install axios --save`
---
+++ _spa/src/App.js
@@ -0,0 +1,58 @@
+import './App.css';
+import axios from 'axios';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+
+function post(title=null, description=null, start_date=null, start_time=null, end_date=null, end_time=null) {
+
+  const data = {
+    'title': title,
+    'description': description,
+    'start_date': start_date,
+    'start_time': start_time,
+    'end_date': end_date,
+    'end_time': end_time
+  };
+
+  const a = axios.get('https://croissant.pythonanywhere.com/layers/')
+  console.log(a)
+
+  const url = axios.post('https://croissant.pythonanywhere.com/layers/', data)
+
+    .then(() => {
+        console.log(url);
+    })
+    
+    .catch(err => {
+        console.log('err:', err);
+    });
+}
+
+
+function App() {
+  return (
+    <div>
+      <TextField id="title" label="Title" variant="outlined" />
+      <TextField id="description" label="Description" variant="outlined" multiline rows="3" />
+      <p>start date</p>
+      <input type="date" id="start-date"></input>
+      <p>start time</p>
+      <input type="time" id="start-time"></input>
+      <p>end date</p>
+      <input type="date" id="end-date"></input>
+      <p>end time</p>
+      <input type="time" id="end-time"></input>
+      <Button variant="contained" onClick={() => post(
+        // title=document.getElementById('title').value,
+        // description=document.getElementById('description').value,
+        // start_date=document.getElementById('start-date').value,
+        // start_time=document.getElementById('start-time').value,
+        // end_date=document.getElementById('end-date').value,
+        // end_time=document.getElementById('end-time').value
+      )}>Create</Button>
+    </div>
+  );
+}
+
+export default App;
