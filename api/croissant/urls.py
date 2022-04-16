8a5ea41f72e1f73ed30e2d4ec900119956b84dd0
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2022-04-16 20:19:07 +0900

    Add url root

--- api/croissant/urls.py
+++ api/croissant/urls.py
@@ -2,21 +2,9 @@ from django.urls import path
 from croissant import views
 
 
-# LIST_METHODS = {
-#     'get': 'list',
-#     'post': 'create',
-# }
-
-# ITEM_METHODS = {
-#     'get': 'retrieve',
-#     'put': 'update',
-#     # 'patch': 'partial_update',
-#     'delete': 'destroy',
-# }
-
-
 urlpatterns = [
+    path('/', views.LayersView.as_view()),
     path('layers/', views.LayersView.as_view()),
     path('layers/<int:pk>/', views.LayerView.as_view()),
-    path('layers/<int:pk>/children/', views.ChildrenView.as_view())
+    path('layers/<int:pk>/children/', views.ChildrenView.as_view()),
 ]
