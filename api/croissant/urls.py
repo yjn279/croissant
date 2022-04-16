a89c46d15fafa90064826b7626940adf30e7b463
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2022-04-16 20:24:40 +0900

    Remove root url

--- api/croissant/urls.py
+++ api/croissant/urls.py
@@ -3,7 +3,6 @@ from croissant import views
 
 
 urlpatterns = [
-    path('/', views.LayersView.as_view()),
     path('layers/', views.LayersView.as_view()),
     path('layers/<int:pk>/', views.LayerView.as_view()),
     path('layers/<int:pk>/children/', views.ChildrenView.as_view()),
