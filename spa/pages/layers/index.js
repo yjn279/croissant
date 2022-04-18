a1981334acfa96f6a65c03789403bce8ce59d7e8
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2022-04-18 19:31:54 +0900

    Create create button

--- spa/pages/layers/index.js
+++ spa/pages/layers/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import IconButton from '@mui/material/IconButton';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+
 import LayersGet from '@components/layers/get';
 import LayersPost from '@components/layers/post';
 
@@ -7,11 +10,15 @@ import LayersPost from '@components/layers/post';
 export default function Layers() {
 
     const [lastCreated, setLastCreated] = React.useState(null);
+    const [open, setOpen] = React.useState(false);
 
     return (
         <>
-            {/* <LayersGet lastCreated={lastCreated} /> */}
-            <LayersPost setLastCreated={setLastCreated} />
+            <LayersGet lastCreated={lastCreated} />
+            <LayersPost setLastCreated={setLastCreated} open={open} setOpen={setOpen} />
+            <IconButton onClick={() => setOpen(true)}>
+                <SpeedDialIcon />
+            </IconButton>
         </>
     )
 }
